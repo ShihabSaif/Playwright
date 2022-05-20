33f5965d8563796edc83d283b9499fdd8bbad597
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2022-05-20 18:04:07 +0600

    automation script for test case 01 to 03

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,16 +1,26 @@
 const { test, expect } = require('@playwright/test');
 
-// test('basic test', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-//   const title = page.locator('.navbar__inner .navbar__title');
-//   await expect(title).toHaveText('Playwright');
-// });
+test('TC_001', async ({ page }) => {
+  await page.goto('https://demo.guru99.com/v4/index.php');
+  const title = page.locator('xpath = /html/body/div[2]/h2');
+  await expect(title).toHaveText('Guru99 Bank');
+});
 
-test('basic test 2', async ({ page }) => {
+test('TC_002', async ({ page }) => {
   await page.goto('https://demo.guru99.com/v4/index.php');
   await page.fill('input[name="uid"]', 'mngr408769');
   await page.fill('input[name="password"]', 'anasAgA');
   await page.locator('xpath = /html/body/form/table/tbody/tr[3]/td[2]/input[1]').click();
-  // const title = page.locator();
-  // await expect(title).toHaveText('Guru99 Bank');
+  const title = page.locator('xpath = /html/body/table/tbody/tr/td/table/tbody/tr[2]/td/marquee');
+  await expect(title).toHaveText("Welcome To Manager's Page of Guru99 Bank");
+});
+
+test('TC_003', async ({ page}) => {
+  await page.goto('https://demo.guru99.com/v4/index.php');
+  await page.fill('input[name="uid"]', 'mngr408769');
+  await page.fill('input[name="password"]', 'anasAgA');
+  await page.locator('xpath = /html/body/form/table/tbody/tr[3]/td[2]/input[1]').click();
+  await page.locator('xpath = /html/body/div[3]/div/ul/li[15]/a').click();
+  const title = page.locator('xpath = /html/body/div[2]/h2');
+  await expect(title).toHaveText('Guru99 Bank');
 });
